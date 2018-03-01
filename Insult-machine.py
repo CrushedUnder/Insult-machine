2b91be33236b67236eae59b63a0ce76a9af6c8a5
CrushedUnder <36950273+CrushedUnder@users.noreply.github.com>
2018-03-01 21:24:19 +1300

    Add files via upload

---
+++ Insult-machine.py
@@ -0,0 +1,9 @@
+#Lists
+nameL=["Jack","Harry"]
+ageL=
+#Inputs
+name=("Please enter your name")
+age=("Please enter your name")
+Job=str(input("Please enter your profession"))
+
+print("Hi welcome to your custom insult generator please fill out the following field to be suitably insulted")
